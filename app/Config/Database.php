10a1e52c6c7d51778b54a7bfa4f992f1a4bc8c3d
Valeria Palacios <79058926+ValeSPalacios@users.noreply.github.com>
2023-09-24 15:20:38 -0300

    Edit views

--- app/Config/Database.php
+++ app/Config/Database.php
@@ -29,7 +29,7 @@ class Database extends Config
         'hostname' => 'localhost',
         'username' => 'root',
         'password' => '',
-        'database' => 'td-espinozaenrique',
+        'database' => 'palacios_valeria',
         'DBDriver' => 'MySQLi',
         'DBPrefix' => '',
         'pConnect' => false,
