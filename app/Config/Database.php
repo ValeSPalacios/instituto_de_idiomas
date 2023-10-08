4e345deb802d5bf21ebbbef808e1692f4610cd84
Jesica Queupumil <jqueupumil.jq@gmail.com>
2023-10-08 07:22:51 -0300

    primer commit

--- app/Config/Database.php
+++ app/Config/Database.php
@@ -29,7 +29,7 @@ class Database extends Config
         'hostname' => 'localhost',
         'username' => 'root',
         'password' => '',
-        'database' => 'palacios_valeria',
+        'database' => 'queupumil_jesica',
         'DBDriver' => 'MySQLi',
         'DBPrefix' => '',
         'pConnect' => false,
